8b0d294a711cf6e94902fc8c6482c812a1f58f77
Diomidis Spinellis <dds@aueb.gr>
2018-12-14 07:28:09 +0000

    Update graph drawing

--- mkgraph.bat
+++ mkgraph.bat
@@ -1,4 +1,5 @@
 ren UmlGraph-Java UmlGraph.java
-javadoc -docletpath /dds/src/Research/umlgraph/lib/UmlGraph.jar -doclet org.umlgraph.doclet.UmlGraph -private -attributes -operations -nodefontsize 14 -nodefillcolor yellow *.java
-dot -Tpng -ograph.png graph.dot
+javadoc -docletpath /dds/src/Research/umlgraph/lib/UmlGraph.jar -doclet org.umlgraph.doclet.UmlGraph -private -attributes -operations -nodefontsize 14 -edgefontsize 14 -nodefillcolor yellow *.java
 ren UmlGraph.java UmlGraph-Java
+dot -Tpng -ograph.png graph.dot
+copy graph.png ..\..\..\..
